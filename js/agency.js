eb9b1b1ccced62b374238afdbea62196a547d174
bhdm <bhd.m@ya.ru>
2016-09-14 14:22:33 +0300

    u

--- js/agency.js
+++ js/agency.js
@@ -1,27 +1,29 @@
-// Agency Theme JavaScript
+    $(document).ready(function () {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            }, 1250, 'easeInOutExpo');
+            event.preventDefault();
+        });
 
-(function($) {
-    "use strict"; // Start of use strict
+        $('#langadd').click(function () {
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    // $('a.page-scroll').bind('click', function(event) {
-    //     var $anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: ($($anchor.attr('href')).offset().top - 50)
-    //     }, 1250, 'easeInOutExpo');
-    //     event.preventDefault();
-    // });
-    //
-    // // Highlight the top nav as scrolling occurs
+            $('.lang-box').append($('.lang-box-hidden').html());
+        });
+    });
+
+
+    // Highlight the top nav as scrolling occurs
     // $('body').scrollspy({
     //     target: '.navbar-fixed-top',
     //     offset: 51
     // });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-    });
+    // // Closes the Responsive Menu on Menu Item Click
+    // $('.navbar-collapse ul li a').click(function(){
+    //         $('.navbar-toggle:visible').click();
+    // });
 
     // Offset for Main Navigation
     // $('#mainNav').affix({
@@ -30,4 +32,3 @@
     //     }
     // })
 
-})(jQuery); // End of use strict
